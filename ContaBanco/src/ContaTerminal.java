a7de8e289ffd8831c7e71f86a8a33ba42470d8fe
Gadiel-S <gadielbarrosdesouza@gmail.com>
2024-11-06 15:16:32 -0300

    Conta Terminal finalizada

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite seu nome e sobrenome!");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int conta = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Por favor, informe o saldo!");
+        double saldo = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
     }
 }
